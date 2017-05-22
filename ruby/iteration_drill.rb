dc7c71e14f80314e06b1f3fd8300503edee06a20
gmmowry <glenna.mowry@devbootcamp.com>
2017-05-22 08:57:55 -0500

    Update 5.7 drill to specify using #each for iteration

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -3,22 +3,22 @@
 zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
                               "shotgun", "compass", "CB radio", "batteries"]
 
-# 1. Iterate through the zombie_apocalypse_supplies array,
+# 1. Iterate through the zombie_apocalypse_supplies array using #each,
 # printing each item in the array separated by an asterisk
 # ----
 
 # 2. In order to keep yourself organized, sort your zombie_apocalypse_supplies
-# in alphabetical order. Do not use the built-in method .sort/.sort!
+# in alphabetical order using #each.
 # ----
 
 # 3. Create a method to see if a particular item (string) is in the
-# zombie_apocalypse_supplies. Do not use the built-in method .include
+# zombie_apocalypse_supplies using #each.
 # For instance: are boots in your list of supplies?
 # ----
 
 # 4. You can't carry too many things, you've only got room in your pack for 5.
 # Remove items in your zombie_apocalypse_supplies in any way you'd like,
-# leaving only 5. Do not use the built-in methods .take, .drop, or .sample
+# leaving only 5, using #each.
 # ----
 
 # 5. You found another survivor! This means you can combine your supplies.
@@ -42,28 +42,31 @@ extinct_animals = {
   "Laysan Crake" => 1923
 }
 
-# 1. Iterate through extinct_animals hash, printing each key/value pair
+# 1. Iterate through extinct_animals hash using #each, printing each key/value pair
 # with a dash in between the key and value, and an asterisk between each pair.
 # ----
 
 # 2. Keep only animals in extinct_animals if they were extinct before
-# the year 2000. Do not use any the built-in methods .delete_if or .keep_if
+# the year 2000, using #each.
 # ----
 
 # 3. Our calculations were completely off, turns out all of those animals went
 # extinct 3 years before the date provided. Update the values in extinct_animals
-# so they accurately reflect what year the animal went extinct.
+# using #each, so they accurately reflect what year the animal went extinct.
 # ----
 
 # 4. You've heard that the following animals might be extinct, but you're not sure.
-# Build a method that checks if an animal is in the hash and returns true/false.
-# Call the method in your driver code to check if the following animals are extinct or not:
+# Build a method  using #each that checks if an animal is in the hash and returns true/false.
+# Call the method in your driver code with each of the following animals to check if
+# they are extinct or not:
 # "Andean Cat"
 # "Dodo"
 # "Saiga Antelope"
-# Do not use the built-in methods .include?, .has_key?, .member?
+# Driver code example: is_extinct?(extinct_animals, "Andean Cat")
 # ----
 
 # 5. We just found out that the Passenger Pigeon is actually not extinct!
 # Remove them from extinct_animals and return the key value pair as a two item array.
+# Find a Ruby Hash built-in method that helps you accomplish this or build
+# your own method using #each
 # ----
