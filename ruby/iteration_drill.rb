65a900aa7ffed191532569842a076e7dfea12162
gmmowry <glenna.mowry@devbootcamp.com>
2017-06-02 10:23:06 -0500

    remove alphabetical array sort question

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -7,21 +7,17 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 # printing each item in the array separated by an asterisk
 # ----
 
-# 2. In order to keep yourself organized, sort your zombie_apocalypse_supplies
-# in alphabetical order using #each.
-# ----
-
-# 3. Create a method to see if a particular item (string) is in the
+# 2. Create a method to see if a particular item (string) is in the
 # zombie_apocalypse_supplies using #each.
 # For instance: are boots in your list of supplies?
 # ----
 
-# 4. You can't carry too many things, you've only got room in your pack for 5.
+# 3. You can't carry too many things, you've only got room in your pack for 5.
 # Remove items in your zombie_apocalypse_supplies in any way you'd like,
 # leaving only 5, using #each.
 # ----
 
-# 5. You found another survivor! This means you can combine your supplies.
+# 4. You found another survivor! This means you can combine your supplies.
 # Create a new combined supplies list out of your zombie_apocalypse_supplies
 # and their supplies below. You should get rid of any duplicate items.
 # Find the built-in method that helps you accomplish this in the Ruby
