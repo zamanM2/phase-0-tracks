3606c4e53b289b3d9f86610e06fd0ca01d9ae3c9
Mohammad Zaman <mohammad.badruzz@gmail.com>
2017-07-24 04:50:32 -0400

    had to directly make changes to git hub, terminal wont push

---
+++ ruby/vampires.rb
@@ -0,0 +1,61 @@
+loys do you want to do?"
+survey = gets.chomp
+while survey < x
+	do
+
+def employee
+
+	wolves_like_sunshine = true
+wolves_like_garlic = true
+vampires_like_sunshine = false
+vampires_like_garlic = false
+
+
+puts "what is your name?"
+name = gets.chomp
+if name == 'Drake Cula' ||'Tu Fang'
+	puts "Deffinetly a Vampire"
+else puts "Results inconclusive"
+
+	puts "How old are you?"
+	old = gets.chomp
+
+	if old >=120
+	
+		puts "when were you born?"
+
+		age = current_year - year
+
+		while old  != age 
+			puts " Probably a vampire"
+
+		puts "Do you like garlic bread?"
+			vampires_like_garlic = false
+			bread = gets.chomp
+			if bread == true 
+				puts "vampire"
+			else puts "not vampire"
+			end
+
+			
+				puts "would you like to sign up for insurance"
+				insurance = gets.chomp
+				insurance == "yes"
+				if insurance == "no"
+					puts "vampire"
+				else puts "not a vampire"
+				
+				if enployee old == age && bread == false || insurance == "yes"
+					puts "probably not a vampire"
+					if else enployee old != age && bread == true || insurance == "no"
+						puts "probably a vampire"
+						if else enployee old != age && bread == true && insurance == "no"
+							puts "almost certainly a vampire"
+							if else name == "Drake Cula" || "Tu Fang"
+								puts "deffinetly a vampire"
+							else print " Results Inconclusive"
+
+
+				end
+
+			end
