36732bebe88cb90c86537a242298936a9a6bc2af
Mohammad Zaman <mohammad.badruzz@gmail.com>
2017-07-24 05:56:18 -0400

    Update secret_agents.rb

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,54 @@
+#set counter to 0 
+#make while loop to repeat
+ #make a funtion to add one yo letter
+ #print the new letter
+
+ def encrypt(string)
+ 	counter = 0
+ 	string = counter.pos
+
+ 	while counter <= 25
+   counter += 1
+  puts string
+  
+  end
+def encrypt(string)
+ 	counter = 0
+ 	string = counter.pos
+
+ 	while counter <= 25
+   counter += 1
+  puts string
+  
+  end
+
+decrypt(encrypt("swordfish")
+# This nested call works because you are essetially just calling the first method which is decrypt and then calling the encrypt methodto put the string back the way it wwas.
+
+
+puts " Would you like to encrypt or decrypt?"
+choice = gets.chomp
+if choise == "encrypt"
+	do
+def encrypt(string)
+ 	counter = 0
+ 	string = counter.pos
+
+ 	while counter <= 25
+   counter += 1
+  puts string
+  
+  end
+
+else do 
+	def encrypt(string)
+ 	counter = 0
+ 	string = counter.pos
+
+ 	while counter <= 25
+   counter += 1
+  puts string
+  
+  end
+
+end 
