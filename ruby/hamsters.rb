94416e381e90edd05d16c3aa611d3f4b945e6741
Mohammad Zaman <mohammad.badruzz@gmail.com>
2017-07-24 02:19:10 -0400

    made methods

---
+++ ruby/hamsters.rb
@@ -0,0 +1,33 @@
+def hamster(name)
+   
+   puts "what is the hamsters name?"
+   name= gets.chomp
+   print name
+   end
+
+def noise(level)
+    
+    puts "what is the noise level?"
+    
+    level= gets.chomp #{x}
+    prints x
+    end
+
+def fur(color)
+    puts "what is the fur color"
+    color = gets.chomp
+    print color
+    
+    end
+
+def adoptv(hamster)
+    puts "is the hamster good for adopt"
+    hamster = gets.chomp
+    print hamster
+    end
+
+def age (hamster)
+puts "what is the hamsters age"
+age = gets.chomp
+print age
+end
