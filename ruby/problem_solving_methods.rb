0b7a99f8344de86915698bce31bd156f95a110f3
Mohammad Zaman <mohammad.badruzz@gmail.com>
2017-08-03 04:03:39 -0400

    updated

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,21 @@
+deff search (array)
+arrray = []
+
+if search_array(array, int=0)
+	p search_array(arrray, int=0)
+
+else p "nill"
+	array =array.length
+
+
+
+
+
+end 
+
+def fib( n )
+     if x <= 1
+     return x 
+   else  fib( x - 1 ) + fib( x - 2 ) =fibbonacci
+   	print fibbinacci
+end 
