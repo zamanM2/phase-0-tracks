b6bb8dbe7f77e1c58ffba5552102dbe997de89ac
Mohammad Zaman <mohammad.badruzz@gmail.com>
2017-08-07 00:56:03 -0400

    i commited all my git workflow to puppy.rb so im recommiting this to the right one

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -7,3 +7,79 @@ class Puppy
 
 end
 
+
+class Puppy
+	def initialize(name,species, behavior)
+		name = " "
+		species = "Dog"
+		chararacteristics 
+		behavior 
+
+	end
+
+	  def fetch(toy)
+    puts "I brought back the #{toy}!"
+    toy
+  end
+  
+  def speak(int )
+  	int = gets.chomp
+ int.times do
+ print "Woof"
+
+end
+  
+  def roll_over
+    prints "Rolls over"
+    
+  end
+  
+  def dog_year(int)
+    human_year = x
+    dog_years= human_year * 15
+    print dog_years
+    
+  end
+end 
+
+end
+
+end
+
+#Driver Code *************************************
+
+
+i = Puppy.new 
+i.roll_over
+
+i = Puppy.new
+i.dog_years
+
+i = Puppy.new 
+i.fetch
+
+i = Puppy.new
+i.pet_belly
+
+
+#New Class *****************************************
+
+
+class Ninja
+  def warriror
+    print "I am a warior!"
+    
+  end
+def clan
+  print "I am from the #{clan_name} clan"
+end
+
+end 
+
+while j <= 50
+ j = Ninja.new
+    j.warior
+    
+     Ninja.each 
+    
+  end
