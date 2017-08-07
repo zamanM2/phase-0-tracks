cc51fc53a1ba432bba5fb47ad33c2de2d8c4d571
Mohammad Zaman <mohammad.badruzz@gmail.com>
2017-08-07 04:37:55 -0400

    added driver code and included two modules

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
   def self.yell_angrily(words)
     words + "!!!" + " :("
@@ -11,3 +12,30 @@ end
 
 #driver ********************
 Shout.yell_angrily(hate)
+=end
+
+module Shout
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+  def yelling_happily(word)
+  	word+ "!" + ":]"
+  end
+
+
+
+
+
+class Old_man
+	include shout
+end
+
+class Auctioner
+	include shout
+end
+
+Old_man = Old_man.new
+Old_man.yell_angrily(QUIET)
+
+Auctioner = Auctioner.new
+Auctioner.yelling_happily(ONEDOLLER)
