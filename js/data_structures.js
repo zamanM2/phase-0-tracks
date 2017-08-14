b644437820eadbfc67c9dc6f882da465270bd5e4
Mohammad Zaman <mohammad.badruzz@gmail.com>
2017-08-14 02:33:22 -0400

    added colors and horses

---
+++ js/data_structures.js
@@ -0,0 +1,14 @@
+var colors = ["green", "blue", "orange" , "pink" ]
+var names = ["Ed", "Sheeren", "Carmello", "Anthony"]
+
+colors.push("red")
+names.push("Kobe")
+
+console.log(colors);
+console.log (names);
+
+var barn = []
+
+for (var x = 0 ; x < colors.length ; x++){
+	barn[horses]= colors[x];
+}
